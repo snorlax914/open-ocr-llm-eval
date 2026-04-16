a6d75984a48a94102cb240571bd97760df61c2d0
snorlax914 <whisu2000@gmail.com>
2026-04-16 18:24:21 +0900

    research: GLM-OCR 테스트 코드 작성

---
+++ results/week1/GLM-OCR/test.py
@@ -0,0 +1,32 @@
+from transformers import AutoProcessor, AutoModelForImageTextToText
+import torch
+
+MODEL_PATH = "zai-org/GLM-OCR"
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {"type": "image", "url": "test_image.png"},
+            {"type": "text", "text": "Text Recognition:"},
+        ],
+    }
+]
+processor = AutoProcessor.from_pretrained(MODEL_PATH, trust_remote_code=True)
+model = AutoModelForImageTextToText.from_pretrained(
+    pretrained_model_name_or_path=MODEL_PATH,
+    torch_dtype="auto",
+    device_map="auto",
+)
+inputs = processor.apply_chat_template(
+    messages,
+    tokenize=True,
+    add_generation_prompt=True,
+    return_dict=True,
+    return_tensors="pt",
+).to(model.device)
+inputs.pop("token_type_ids", None)
+generated_ids = model.generate(**inputs, max_new_tokens=8192)
+output_text = processor.decode(
+    generated_ids[0][inputs["input_ids"].shape[1] :], skip_special_tokens=False
+)
+print(output_text)
